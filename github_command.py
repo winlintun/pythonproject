991b80a1602f4c54d18d453a742f5f8a050e8973
iceface <www.iceface@gmail.com>
2021-11-14 19:13:10 +0630

    github command txt

---
+++ github_command.py
@@ -0,0 +1,42 @@
+print("""
+
+
+1>git config --global user.name 'iceface'
+2>git config user.email 'www.iceface@gmail.com'
+3>git config user.name # check username
+4> git config user.email # check email
+create  repo
+5>git init 
+exist projectfile using git
+6>git init
+show modif
+7>git status
+remove modif
+8>git rm --cache (filename)
+#git add many file and many folder
+9>git add .
+10>git commit -m 'text hrer'
+#see commit
+11>git log
+12>git log --online
+13>
+1.Checkout commit
+2.Revert commit
+3.Rest commit
+
+14>git checkout (read only)
+and 
+15> git checkout master
+16>git revert (id here) --hard  # safe delete
+#master branch
+17>git branch -a # see branch
+18>git branch feature-1 # create feature-1 branch
+19>git checkout feature-1 # select feature-1 branch
+20>git checkout -b feature-2 # create new brach and select this branch
+21>git branch -D # fist go master branch and delete branch another branch
+22>git merge (branch file) #first go master and marge branch to master branch
+
+
+
+""")
+
