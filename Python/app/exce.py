97da9d1e12efebc1e813285ce7a521952bcb0d72
iceface <www.iceface@gmail.com>
2021-11-28 22:12:19 +0630

    complet website

--- Python/app/exce.py
+++
@@ -1,9 +0,0 @@
-import openpyxl as xl
-
-wb = xl.load_workbook('data.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['a1'] # a = column and 1 = row
-cell = sheet.cell(1,1) # 1 = row , 1 = column
-
-print(cell.value)
-
--- Python/app/random.py
+++
@@ -1,5 +0,0 @@
-import random
-
-lst = ['Mg', 'Ma', 'Ko']
-member = random.choice(lst)
-print(member)
